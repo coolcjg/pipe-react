27319700e57793b9ebcfb648f12b254914b45883
coolcjg <coolcjg@naver.com>
2023-02-01 23:31:12 +0900

    init

---
+++ src/view/list.js
@@ -0,0 +1,9 @@
+const List = () => {
+    return (
+        <>
+            <h3>리스트</h3>
+        </>
+    )
+}
+
+export default List;
---
+++ src/view/main.js
@@ -0,0 +1,9 @@
+const Main = () => {
+    return (
+        <>
+            <h3>메인 페이지</h3>
+        </>
+    );
+};
+
+export default Main;
