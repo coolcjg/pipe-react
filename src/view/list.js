83fd739fb662f5aef3417ddc5d5286d22d1b67d8
coolcjg <coolcjg@naver.com>
2023-02-02 23:27:01 +0900

    'axios'

--- src/view/list.js
+++ src/view/list.js
@@ -1,4 +1,27 @@
+import axios from "axios";
+import { useEffect } from "react";
+
 const List = () => {
+
+    const baseUrl = 'http://localhost:8080/userList';
+
+
+    useEffect(() => {
+        userList();
+        console.log("실행");
+    }, []);
+
+
+    async function userList() {
+        await axios.get(baseUrl)
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((error) => {
+                console.error(error);
+            })
+    }
+
     return (
         <>
             <h3>리스트</h3>
