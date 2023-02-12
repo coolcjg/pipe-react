bf9f6d18dc8271453d2ea30a0f7e7376d8868a4b
coolcjg <coolcjg@naver.com>
2023-02-12 21:01:59 +0900

    checkbox complete

--- src/view/list.js
+++ src/view/list.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
-import UserList from './userList';
+export const SERVER_DOMAIN = `${process.env.REACT_APP_SERVER_DOMAIN}`
 
 const List = () => {
-
-    const baseUrl = 'http://localhost:8080/userList';
+    const baseUrl = SERVER_DOMAIN + '/userList';
 
     const [userList, setUserList] = useState([]);
+    const [checkItems, setCheckItems] = useState([]);
 
     useEffect(() => {
         userListSelect();
@@ -23,22 +23,71 @@ const List = () => {
             })
     }
 
+    const goWriteForm = () => {
+        document.location.href = '/write';
+    }
+
+    async function deleteUser() {
+        await axios.post(SERVER_DOMAIN + '/deleteUser', {
+            userIdArray: ''
+        })
+            .then((response) => {
+                console.log(response);
+                setUserList(response.data.list);
+            })
+            .catch((error) => {
+                console.error(error);
+            })
+    }
+
+    const checkAll = (checked) => {
+
+        if (checked === true) {
+            const userIdArray = [];
+            userList.forEach((el) => userIdArray.push(el.userId));
+            setCheckItems(userIdArray);
+        } else {
+            setCheckItems([]);
+        }
+
+    }
+
+    const checkSingle = (userId, checked) => {
+        if (checked) {
+            setCheckItems(prev => [...prev, userId]);
+
+        } else {
+            setCheckItems(checkItems.filter((el) => el !== userId));
+        }
+    };
+
     return (
         <>
             <h3>리스트</h3>
             <table className="userListTable">
                 <thead>
                     <tr>
-                        <th><input type="checkbox" /></th>
+                        <th><input type="checkbox" onChange={(e) => checkAll(e.target.checked)} checked={checkItems.length === userList.length ? true : false} /></th>
                         <th>아이디</th>
                         <th>이름</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <UserList userList={userList} />
+                    {
+                        userList.map((user) =>
+                            <tr key={user.userId}>
+                                <td><input type="checkbox" name="userCheckBox" checked={checkItems.includes(user.userId)} onChange={(e) => checkSingle(user.userId, e.target.checked)} /></td>
+                                <td>{user.userId}</td>
+                                <td>{user.userName}</td>
+                            </tr>
+                        )
+                    }
                 </tbody>
             </table>
-
+            <div>
+                <button onClick={goWriteForm}>등록</button>
+                <button onClick={deleteUser}>삭제</button>
+            </div>
 
         </>
     )
