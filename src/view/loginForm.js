142b5151a21c54757331162b842dcadb0d15ad94
coolcjg <coolcjg@naver.com>
2023-03-06 22:06:18 +0900

    jwt 작업중

--- src/view/loginForm.js
+++ src/view/loginForm.js
@@ -21,10 +21,16 @@ const LoginForm = () => {
 
         const encryptPassword = sha256(inputs.password);
 
+        console.log("inputs");
+        console.log(inputs);
+
         axios.post('http://localhost:8080/login', {
             userId: inputs.userId
             , password: encryptPassword
         }).then((response) => {
+
+            console.log(response);
+
 
 
         }).catch((error) => {
@@ -33,15 +39,21 @@ const LoginForm = () => {
 
     }
 
+    const handleOnKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            login();
+        }
+    }
+
     return (
         <>
             <div>
                 <h1>로그인</h1>
                 <div>
-                    <input type="text" id="userId" name="userName" placeholder="아이디" onChange={handleChange} />
+                    <input type="text" id="userId" name="userId" placeholder="아이디" onChange={handleChange} />
                 </div>
                 <div>
-                    <input type="password" id="password" name="password" placeholder="비밀번호" onChange={handleChange} />
+                    <input type="password" id="password" name="password" placeholder="비밀번호" onChange={handleChange} onKeyUp={handleOnKeyPress} />
                 </div>
                 <button onClick={login}>로그인</button>
             </div>
